d5fe72de9b888038b815c4443e6c15ba5ee0f544
j <rocketmortonspage@aol.com>
2021-12-30 13:06:08 -0500

    fixbuttons was unnecessary, redefining jquery somehow broke them in the first place and changing the definition to the sites fixed it

--- fixbuttons.js
+++
@@ -1,9 +0,0 @@
-// OUTSIDE
-var rymQre = /rymQ\(\s*function\(\)\s*{\s*(.*)\s*}\s*\)/g;
-
-// INSIDE
-$("[onclick^=rymQ]").attr("onclick", function(i, value) {
-	// this looks scary refer to this 
-	// https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
-	return [...value.matchAll(rymQre)].map(m => m[1])[0].trim();
-});
