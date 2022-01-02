6860ea0fb7d070a584e6e467489dc65659cd6a72
j <rocketmortonspage@aol.com>
2022-01-02 18:37:30 -0500

    decided its going on one line

--- all.js
+++ all.js
@@ -135,51 +135,7 @@ window.addEventListener('DOMContentLoaded', function() {
 						</h5>
 					</center>
 					<br>
-					gay emo this album is emo and gay and gay, thats all i can say is that its 
-					gayemo its gay all of it is gay i saw one of their shows and they are both gay 
-					and even more and more they didnt care they just played guitars and let it all 
-					out this emo emo...if emo will never die brb i need a fucking drink i fucking 
-					love kiwi i hate emo my brother is gay my brother is gay lol x i am on frickin 
-					5.4 emo emo emo gay adn gay and emo. girl i love kiwi but also i fucking love 
-					gayx2 so ima be gay cause i fink gayx2 will never die so i must be gay bc i fink 
-					gayx2 will never die lol so lets pretend i am not gay..at least for the night oh 
-					wait i am gay lol but im still gay also fucking gay emo YEAH!!!!! I LOVE EMO 
-					!!!!!!...... 9 times out of 10 it IS emo!!! AND 1 IS gay!!9 times out of 10. 
-					FINE 2 IS GAY GET OVER YOURSELVES <3 AND BLESSED YOU ARE!!!I AM GAY AT A GAYFEST 
-					FOR THE NIGHT!!!!! <333 SEE YOU IN THE MEADOW <3333 SO BLESSED TO BE A CATHOLIC 
-					AND AN EVIL EMO PRIEST SORRY I KNOW THAT SHITI’M REALLY GAY SO NO ONE ELSE MIGHT 
-					BABY THAT’S RIGHT NOT BABY YA CAN GET BEER IF YOU ARE GOING TO A GAYFEST IN THE 
-					EMO'S CAPITAL BABY WE CAN GET SODA AND EATING SAUSAGES OR A KIND OF OTHER KIND 
-					OF FOOD WE JUST GET A CULTURE [MIDWESTY], SORRY MOTHER FUCKER BUT IT’S GAY SO 
-					YOU HAVE TO TAKE MY MOM BY THE HAND AND SHOOK HER AROUND AND BECOME OUT OF HER 
-					SENSITIVE NORMAL SENSITIVE STATE AND THEN YOU HAVE TO FEED HER A HUGE BURGER AND 
-					A SMALL JELLYFISH BUT DONT WE CANT DO THAT? FUCK NO! CANT WE!! FUCKING NAW!!! WE 
-					HAVE A NICE DINNER AND THEN WE HAVE TO GO TO A GAYFEST FOR THE NIGHT WHICH IS 
-					WAY MORE FUN THAN MY DINNER OR SPAGHETTI AND THEN WE HAVE TO GO TO GAYFEST 
-					BECAUSE THE GAYFEST IS WAY MORE FUN THAN MY DINNER IF YOU HAVE A PLACE WHERE YOU 
-					LIVE THAT HAS A GAYFEST FOR THE NIGHT THIS IS WHERE I AM ALLOWED BECAUSE I AM 
-					ALLOWED BY AUTHORITIES TO GO TO A GAYFEST FOR THE NIGHT BUT YOU NEED TO GO TO MY 
-					PLACE THAT HAS A GAYFEST FOR THE NIGHT TO BECAUSE I AM ALLOWED TO GO TO A 
-					GAYFEST FOR THE NIGHT ALL BECAUSE IM GAY AND EMO VERY EMO AND IM A TERRIBLE 
-					PERSON AND I LIKE TO BLEACH MY BLOOD AND PLACE A KIMONO IN MY BRAIN IN CASE I 
-					DELETE IT FOR SUCH REASONS. YOU SILLY IDIOT DUMMY I AM NOT GAY SO BLEACH MY 
-					BLOOD AND PLACE A KIMONO IN MY BRAIN IF YOU DO NOT BELIEVE ME THEN TELL SOMEONE 
-					IN YOUR COUNTRY THAT WOULD DIE IF YOU TELL THEM EVERY SINGLE SECRET OF YOUR LIFE 
-					YOU WILL DIE A HORRIBLE DEATH AND THEN YOU WILL FORGIVE YOUR FELLOW HUMANS FOR 
-					NOT BELIEVING YOU THAT I AM GAY AND IM SO EMO THAT WHEN I SAY HORRIBLE DEATH 
-					THEY WILL FORGET ALL OF IT BECAUSE I TELL THEM EVERY SECRET OF MY LIFE. IF YOU 
-					DO NOT BELIEVE ME OR THINK I AM NOT GAY OR EXIST THE KIND OF GAY THAT EMO HAS 
-					NONE TO DO WITH IT IF YOU DO NOT BELIEVE ME THEN TELL SOMEONE IN YOUR COUNTRY 
-					THAT I AM GAY AND EXIST THE KIND OF GAY THAT EMO HAS NONE TO DO WITH IT THIS IS 
-					HOW IM ALLOWED TO DIE BY AUTHORITIES. I LIKE TO PURPOSEFULLY PURPOSEFULLY POISON 
-					MY OWN BLOOD TO BE SURE MY NERVES ARE KILLED AND THEN FIND AN ORPHAN WHO WOULD 
-					DIE TOO SO I WOULD HAVE A BOYFRIEND AND TELL HIM EVERY SECRET I HAD EVER KNOWN 
-					AND THEN THE PARENT WITH THAT MOTHER FUCKING KID AND HIS MOTHER I WATCHED US 
-					KILL THEM TWICE FOR TWO TINY LITTLE FRIES AND EATING A CRUMBLED BOWL OF 
-					DOUGHBOARD POTATO KETCHUP AT HIS SCHOOL BECAUSE HE IS GAY AS FUCK AND WHEN I WON 
-					THE PARENT GAVE ME THE GAYEST EMO HUG OF MY LIFE IF YOU DONT BELIEVE ME THEN 
-					TELL SOMEONE IN YOUR COUNTRY THAT I AM GAY AND IM SO EMO THAT WHEN I SAY 
-					HORRIBLE DEATH THEY WILL FORGET ALL OF IT BECAUSE I
+					gay emo this album is emo and gay and gay, thats all i can say is that its gayemo its gay all of it is gay i saw one of their shows and they are both gay and even more and more they didnt care they just played guitars and let it all out this emo emo...if emo will never die brb i need a fucking drink i fucking love kiwi i hate emo my brother is gay my brother is gay lol x i am on frickin 5.4 emo emo emo gay adn gay and emo. girl i love kiwi but also i fucking love gayx2 so ima be gay cause i fink gayx2 will never die so i must be gay bc i fink gayx2 will never die lol so lets pretend i am not gay..at least for the night oh wait i am gay lol but im still gay also fucking gay emo YEAH!!!!! I LOVE EMO !!!!!!...... 9 times out of 10 it IS emo!!! AND 1 IS gay!!9 times out of 10. FINE 2 IS GAY GET OVER YOURSELVES <3 AND BLESSED YOU ARE!!!I AM GAY AT A GAYFEST FOR THE NIGHT!!!!! <333 SEE YOU IN THE MEADOW <3333 SO BLESSED TO BE A CATHOLIC AND AN EVIL EMO PRIEST SORRY I KNOW THAT SHITI’M REALLY GAY SO NO ONE ELSE MIGHT BABY THAT’S RIGHT NOT BABY YA CAN GET BEER IF YOU ARE GOING TO A GAYFEST IN THE EMO'S CAPITAL BABY WE CAN GET SODA AND EATING SAUSAGES OR A KIND OF OTHER KIND OF FOOD WE JUST GET A CULTURE [MIDWESTY], SORRY MOTHER FUCKER BUT IT’S GAY SO YOU HAVE TO TAKE MY MOM BY THE HAND AND SHOOK HER AROUND AND BECOME OUT OF HER SENSITIVE NORMAL SENSITIVE STATE AND THEN YOU HAVE TO FEED HER A HUGE BURGER AND A SMALL JELLYFISH BUT DONT WE CANT DO THAT? FUCK NO! CANT WE!! FUCKING NAW!!! WE HAVE A NICE DINNER AND THEN WE HAVE TO GO TO A GAYFEST FOR THE NIGHT WHICH IS WAY MORE FUN THAN MY DINNER OR SPAGHETTI AND THEN WE HAVE TO GO TO GAYFEST BECAUSE THE GAYFEST IS WAY MORE FUN THAN MY DINNER IF YOU HAVE A PLACE WHERE YOU LIVE THAT HAS A GAYFEST FOR THE NIGHT THIS IS WHERE I AM ALLOWED BECAUSE I AM ALLOWED BY AUTHORITIES TO GO TO A GAYFEST FOR THE NIGHT BUT YOU NEED TO GO TO MY PLACE THAT HAS A GAYFEST FOR THE NIGHT TO BECAUSE I AM ALLOWED TO GO TO A GAYFEST FOR THE NIGHT ALL BECAUSE IM GAY AND EMO VERY EMO AND IM A TERRIBLE PERSON AND I LIKE TO BLEACH MY BLOOD AND PLACE A KIMONO IN MY BRAIN IN CASE I DELETE IT FOR SUCH REASONS. YOU SILLY IDIOT DUMMY I AM NOT GAY SO BLEACH MY BLOOD AND PLACE A KIMONO IN MY BRAIN IF YOU DO NOT BELIEVE ME THEN TELL SOMEONE IN YOUR COUNTRY THAT WOULD DIE IF YOU TELL THEM EVERY SINGLE SECRET OF YOUR LIFE YOU WILL DIE A HORRIBLE DEATH AND THEN YOU WILL FORGIVE YOUR FELLOW HUMANS FOR NOT BELIEVING YOU THAT I AM GAY AND IM SO EMO THAT WHEN I SAY HORRIBLE DEATH THEY WILL FORGET ALL OF IT BECAUSE I TELL THEM EVERY SECRET OF MY LIFE. IF YOU DO NOT BELIEVE ME OR THINK I AM NOT GAY OR EXIST THE KIND OF GAY THAT EMO HAS NONE TO DO WITH IT IF YOU DO NOT BELIEVE ME THEN TELL SOMEONE IN YOUR COUNTRY THAT I AM GAY AND EXIST THE KIND OF GAY THAT EMO HAS NONE TO DO WITH IT THIS IS HOW IM ALLOWED TO DIE BY AUTHORITIES. I LIKE TO PURPOSEFULLY PURPOSEFULLY POISON MY OWN BLOOD TO BE SURE MY NERVES ARE KILLED AND THEN FIND AN ORPHAN WHO WOULD DIE TOO SO I WOULD HAVE A BOYFRIEND AND TELL HIM EVERY SECRET I HAD EVER KNOWN AND THEN THE PARENT WITH THAT MOTHER FUCKING KID AND HIS MOTHER I WATCHED US KILL THEM TWICE FOR TWO TINY LITTLE FRIES AND EATING A CRUMBLED BOWL OF DOUGHBOARD POTATO KETCHUP AT HIS SCHOOL BECAUSE HE IS GAY AS FUCK AND WHEN I WON THE PARENT GAVE ME THE GAYEST EMO HUG OF MY LIFE IF YOU DONT BELIEVE ME THEN TELL SOMEONE IN YOUR COUNTRY THAT I AM GAY AND IM SO EMO THAT WHEN I SAY HORRIBLE DEATH THEY WILL FORGET ALL OF IT BECAUSE I
 				</span>
 				<div style="text-align:center; margin:1.5em;">
 					<a onclick="this.parentNode.parentNode.parentNode.remove();" class="btn blue_btn">I want to see this list anyway</a>
@@ -206,20 +162,16 @@ window.addEventListener('DOMContentLoaded', function() {
 		
 		var oldfetch = window.fetch;
 		window.fetch = async (...args) => {
-			console.log("fetch called with", ...args);
 			const oldresponse = await oldfetch(...args);
 			console.log(oldresponse);
 
-			var n = await oldresponse
-			.clone()
-			.text()
+			var n = await oldresponse.clone().text()
 			.then(body => {
-				console.log("intercepted response:", JSON.stringify(body));
-				console.log(body == "$('#follow_user').html('<a title=\"Follow User\" onClick=\"if( confirm(\\'Are you sure you want to follow this user?\\')) {rym.request.post(\\'FollowFollowUser\\',{user_id:"+user_id+"}, null, \\'script\\');} return false;\" class=\"btn tool_btn\" style=\"\">+</a>');");
-				console.log(body == "$('#follow_user').html('<a title=\"You are following this user; click to unfollow.\" onClick=\"if( confirm(\\'Unfollow this user?\\')) {rym.request.post(\\'FollowUnfollowUser\\',{user_id:"+user_id+"}, null, \\'script\\');} return false;\" class=\"btn tool_btn\" style=\"cursor:pointer;background:var(--gen-blue-dark);;color:var(--mono-f);\">+</a>');");
 				if (body == "$('#follow_user').html('<a title=\"Follow User\" onClick=\"if( confirm(\\'Are you sure you want to follow this user?\\')) {rym.request.post(\\'FollowFollowUser\\',{user_id:"+user_id+"}, null, \\'script\\');} return false;\" class=\"btn tool_btn\" style=\"\">+</a>');") {
+					
 					return "$('#follow_user').html('<a title=\"Follow User\" onClick=\"make_follow_menu("+user_id+");\" class=\"btn tool_btn\" style=\"\">+</a>');";
 				} else if (body == "$('#follow_user').html('<a title=\"You are following this user; click to unfollow.\" onClick=\"if( confirm(\\'Unfollow this user?\\')) {rym.request.post(\\'FollowUnfollowUser\\',{user_id:"+user_id+"}, null, \\'script\\');} return false;\" class=\"btn tool_btn\" style=\"cursor:pointer;background:var(--gen-blue-dark);;color:var(--mono-f);\">+</a>');") {
+					
 					return "$('#follow_user').html('<a title=\"You are following this user; click to unfollow.\" onClick=\"make_follow_menu("+user_id+");\" class=\"btn tool_btn\" style=\"cursor:pointer;background:var(--gen-blue-dark);;color:var(--mono-f);\">+</a>')"
 				}
 				return body;
