c3d5d3fc7391a1eb472b27241d32d7173bd2977a
j <rocketmortonspage@aol.com>
2022-01-04 01:56:51 -0500

    formatting

--- nofeatured.js
+++ nofeatured.js
@@ -1,4 +1,7 @@
 // INSIDE
+/*
+ * REMOVE FEATURED REVIEWS
+ * * * * * * * * * * * * * */
 // thank you jquery
 if (window.location.href.includes("rateyourmusic.com/release")) {
 	$(".review:has(.review_featured)").remove(); // lol maybe move it instead
