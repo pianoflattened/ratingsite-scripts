0cc0ff9b313574fd2f5ec10e2454ff1280256532
j <rocketmortonspage@aol.com>
2022-01-04 20:33:15 -0500

    works on blocked pages

--- usernotes.js
+++ usernotes.js
@@ -1,10 +1,10 @@
 // OUTSIDE
-var update_user_notes = function() {
+var update_user_notes = function(username) {
 	ldb.get("__notes", function(v) {
 		if (v === null) {
 			v = {};
 		}
-		v[$('span#profilename').text()] = $("textarea#user_notes").val();
+		v[username] = $("textarea#user_notes").val();
 		ldb.set("__notes", v);
 	});
 }
@@ -17,17 +17,26 @@ if (window.location.href.includes("://rateyourmusic.com/~")) {
 	window.update_user_notes = update_user_notes;
 	
 	if ($("div#allNotifications").length == 0) {
+		let spl = window.location.href.split("~");
+		let username = spl[spl.length-1];
 		let notes = $(`<div style="margin: 0 0.5em; display: flex; justify-content: space-between;">
 			<span>notes</span>
 			<span style="font-weight: bold;">[<a onclick="$('textarea#user_notes').is(':disabled') ? $('textarea#user_notes').removeAttr('disabled') : $('textarea#user_notes').attr('disabled', '');">edit</a>]</span>
 		</div>
 		<div class="venuebox" style="padding: 0; border: 1px var(--mono-c) solid;">
-			<textarea disabled id="user_notes" oninput="window.update_user_notes();"></textarea>
+			<textarea disabled id="user_notes" oninput="window.update_user_notes('`+username+`');"></textarea>
 		</div>`);
 		
-		ldb.get("__notes", function(v) {
-			notes.find("textarea#user_notes").val(v[$("span#profilename").text()]);
-			$("td:has(div.profilehii) + td div").append(notes);
-		});
+		if ($("div.note:has(a#block)").length > 1) {
+			ldb.get("__notes", function(v) {
+				notes.find("textarea#user_notes").val(v[username]);
+				$("div.note:has(a#block)").parent().append(notes);
+			});
+		} else {
+			ldb.get("__notes", function(v) {
+				notes.find("textarea#user_notes").val(v[username]);
+				$("td:has(div.profilehii) + td div").append(notes);
+			});
+		}
 	}
 }
