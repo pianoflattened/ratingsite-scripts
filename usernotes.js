89d5ad31e802b177a04c42527168db4a86d3e70e
j <rocketmortonspage@aol.com>
2022-01-04 21:17:35 -0500

    looks cooler

--- usernotes.js
+++ usernotes.js
@@ -19,19 +19,20 @@ if (window.location.href.includes("://rateyourmusic.com/~")) {
 	if ($("div#allNotifications").length == 0) {
 		let spl = window.location.href.split("~");
 		let username = spl[spl.length-1];
-		let notes = $(`<div style="margin: 0 0.5em; display: flex; justify-content: space-between;">
+		let notes = $(`<div class="notes_container" style="margin: 0 0.5em; display: flex; justify-content: space-between;">
 			<span>notes</span>
 			<span style="font-weight: bold;">[<a onclick="$('textarea#user_notes').is(':disabled') ? $('textarea#user_notes').removeAttr('disabled') : $('textarea#user_notes').attr('disabled', '');">edit</a>]</span>
 		</div>
-		<div class="venuebox" style="padding: 0; border: 1px var(--mono-c) solid;">
+		<div class="venuebox notes_container" style="padding: 0; border: 1px var(--mono-c) solid;">
 			<textarea disabled id="user_notes" oninput="window.update_user_notes('`+username+`');"></textarea>
 		</div>`);
 		
 		if ($("div.note:has(a#block)").length >= 1) {
 			ldb.get("__notes", function(v) {
-				notes.css("max-width", "33%;");
 				notes.find("textarea#user_notes").val(v[username]);
 				$("div.note:has(a#block)").parent().append(notes);
+				$("div.notes_container").css("max-width", "33%");
+				$("div.notes_container:first").css("margin-top", "1em");
 			});
 		} else {
 			ldb.get("__notes", function(v) {
