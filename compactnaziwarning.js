9a70d6a193f8bbe5e3e0fa4f905ed03dda8131e3
j <rocketmortonspage@aol.com>
2022-02-15 14:34:16 -0500

    cnw broken, fixing later

--- compactnaziwarning.js
+++ compactnaziwarning.js
@@ -1,20 +1,22 @@
 window.addEventListener('DOMContentLoaded', function() {
-	let t = $("div.warning").text().trim().split("\n");
-	let a = t.splice(0, 1).concat(t.map(n => n+"<br>").join("\n")).filter(n => n);
+	if (false) {// TODO: check if is release page
+		let t = $("div.warning").text().trim().split("\n");
+		let a = t.splice(0, 1).concat(t.map(n => n+"<br>").join("\n")).filter(n => n);
 
-	let warning_dropdown = `if ($('#warning-content').css('display') == 'none') {
-		$('#warning-content').css('display', 'block');
-		$('.warning').css('background-color', 'var(--gen-bg-yellow)');
-	} else {
-		$('#warning-content').css('display', 'none');
-		$('.warning').css('background-color', 'rgba(70,60,7,0.75)'); 
-	}`.split("\n").map(t => t.trim()).join("");
-	
-	if (a.length > 0) {
-		a[0] = "<div id=\"warning-title\">"+a[0]+`<a onclick="`+warning_dropdown+`">▼</a></div>`
-		a[1] = "<div id=\"warning-content\" style=\"display:none;\">"+a[1]+"</div>"
+		let warning_dropdown = `if ($('#warning-content').css('display') == 'none') {
+			$('#warning-content').css('display', 'block');
+			$('.warning').css('background-color', 'var(--gen-bg-yellow)');
+		} else {
+			$('#warning-content').css('display', 'none');
+			$('.warning').css('background-color', 'rgba(70,60,7,0.75)'); 
+		}`.split("\n").map(t => t.trim()).join("");
+		
+		if (a.length > 0) {
+			a[0] = "<div id=\"warning-title\">"+a[0]+`<a onclick="`+warning_dropdown+`">▼</a></div>`
+			a[1] = "<div id=\"warning-content\" style=\"display:none;\">"+a[1]+"</div>"
+		}
+
+		$("div.warning").css("background-color", "rgba(70,60,7,0.75)");
+		$("div.warning")[0].innerHTML = a.join("");
 	}
-
-	$("div.warning").css("background-color", "rgba(70,60,7,0.75)");
-	$("div.warning")[0].innerHTML = a.join("");
 });
